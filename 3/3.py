dd4edc66946e4e1e9ad8aac1c5979309df4a8c46
uczeń <ninka513@o2.pl>
2026-02-23 16:34:01 +0100

    gf1

---
+++ 3/3.py
@@ -0,0 +1,7 @@
+aa=dict()
+aa["imie"] = input("Podaj swoje imię: ")
+aa["wiek"] = int(input("Podaj swój wiek: "))
+aa["miasto"] = input("Podaj miasto, w którym mieszkasz: ")
+
+print(aa)
+print(aa["imie"])
