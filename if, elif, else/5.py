179445d3b1722cb6a584744fef52498915bdb64f
uczeń <ninka513@o2.pl>
2026-03-02 16:37:52 +0100

    gf1

--- if, elif, else/5.py
+++ if, elif, else/5.py
@@ -2,10 +2,12 @@ from os import access
 
 try:
     wiek=int(input("Podaj swój wiek: "))
-access(wiek)
-if wiek >= 18 and wiek < 35:
-    print("Możesz głosować.")
-elif wiek >= 35:
-    print("Możesz nawet zostać prezydentem.")
-else:
-    print("Nie możesz głosować.")
+except:
+    print("Nie to, matma u ma byc")
+if wiek is not None:
+    if wiek >= 18 and wiek < 35:
+        print("Możesz głosować.")
+    elif wiek >= 35:
+        print("Możesz nawet zostać prezydentem.")
+    else:
+        print("Nie możesz głosować.")
