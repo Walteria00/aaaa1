929f1277408eaa70955e374a7f14648ceedba620
uczeń <ninka513@o2.pl>
2026-03-02 16:16:35 +0100

    gf1

--- if, elif, else/8.py
+++ if, elif, else/8.py
@@ -1,7 +1,19 @@
 licznik = 0
-while licznik < 10:
+while licznik <= 10:
     print(licznik)
     licznik += 1
-    if licznik == 10:
+if licznik <= 10:
         print(licznik)
-        licznik = 0
+        licznik += 2
+if licznik == 10:
+    licznik = 0
+    while licznik <= 10:
+        print(licznik)
+        licznik += 5
+if licznik == 10:
+    licznik = 0
+    while licznik <= 10:
+        print(licznik)
+        licznik += 10
+if licznik == 10:
+    licznik = 0
