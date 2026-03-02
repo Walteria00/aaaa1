e59ad7ab7136965f613c894f96b2fdde744f56cf
uczeń <ninka513@o2.pl>
2026-03-02 16:41:10 +0100

    gf1

--- if, elif, else/5.py
+++ if, elif, else/5.py
@@ -1,11 +1,11 @@
 try:
     wiek=int(input("Podaj swój wiek: "))
 except:
-    print("Nie to, matma u ma byc")
+    print("Nie to, matma u ma być, nie umiesz w liczby.")
 if wiek is not None:
     if wiek >= 18 and wiek < 35:
-        print("Możesz głosować.")
+        print("Umiesz w liczby i możesz głosować.")
     elif wiek >= 35:
-        print("Możesz nawet zostać prezydentem.")
+        print("Umiesz w liczby i możesz teraz zostać prezydentem.")
     else:
-        print("Nie możesz głosować.")
+        print("U miesz w liczby ale nie możesz głosować.")
