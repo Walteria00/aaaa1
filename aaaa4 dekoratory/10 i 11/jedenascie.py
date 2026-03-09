ccf7599ba6bd767c6646a7fc3816cc935c4d9bca
uczeń <ninka513@o2.pl>
2026-03-09 16:26:46 +0100

    gf1

--- aaaa4 dekoratory/10 i 11/jedenascie.py
+++ aaaa4 dekoratory/10 i 11/jedenascie.py
@@ -3,7 +3,9 @@ import time
 import dziesiec
 
 @dziesiec.zmierz_czas
-def wolna_funkcja(ar=2,ha=[1,2,3]):
+def wolna_funkcja(ar=2,ha=None):
+    if ha == None:
+        ha = []
     time.sleep(1)
     print("Funkcja zakończona")
     ha.append(ar)
@@ -12,5 +14,5 @@ wolna_funkcja()
 
 if __name__ == "__main__":
     wolna_funkcja()
-    wolna_funkcja(1, [])
+    wolna_funkcja(1, [46,95])
     wolna_funkcja(1)
